55a154b9c0e3f6abbfddf1335234d6e11dcbe4c0
candasilva_meli <caique.ndasilva@mercadolivre.com>
2026-07-28 19:01:27 -0300

    Desafio aula 1 concluido

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Desafio 1 - Aula Generics
 
 Projeto Spring Boot para estudo de Generics em Java.
+
+---
+
+## O que aprendemos?
+
+### Estrutura de um Projeto Spring
+Vimos a estrutura inicial de um projeto Spring e discutimos sobre os pacotes, classes e o método `run`.
+
+### Inferência de tipos no Java
+Vimos um exemplo prático de inferência de tipos com `var` no código Java.
+
+### Consumo de API
+Aprendemos a consumir APIs através do método `obterDados`, que retorna os dados desejados no formato JSON.
+
+### Modularização de código
+Aprendemos a importância de ter um código modularizado e de fácil manutenção.
+
+### Serialização e Desserialização
+Aprendemos como transformar JSON em classes e como isso é útil para o projeto.
+
+### Criação de interfaces e implementação de métodos
+Foi demonstrada a criação de uma interface com um método genérico que usa Generics, bem como a implementação deste método em uma classe separada.
+
+### Inclusão de novas dependências no Projeto
+Vimos como adicionar uma nova dependência ao arquivo `pom.xml` e como esse processo é gerenciado pelo Maven.
--- src/main/java/com/br/alura/ContadorApplication.java
+++ src/main/java/com/br/alura/ContadorApplication.java
@@ -1,9 +1,12 @@
 package com.br.alura;
 
+import com.br.alura.model.Tarefa;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -15,6 +18,14 @@ public class ContadorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Tarefa tarefaLida = objectMapper.readValue(new File("tarefa.json"), Tarefa.class);
+		System.out.println("Tarefa lida do JSON:");
+		System.out.println(tarefaLida);
+	}
+
+	/*@Override
+	public void run(String... args) throws Exception {
 		System.out.println("Bem-vindo ao Contador");
 		System.out.println("========================");
 		System.out.println("Digite um numero:");
@@ -24,5 +35,6 @@ public class ContadorApplication implements CommandLineRunner {
 		for(int i = 1; i <= numero; i++){
 			System.out.print(i + " ");
 		}
-	}
+
+	}*/
 }
